9a8be762d3497c3f21c4b900b58cb1f177fcf0e5
nyan-cat-for-you <164780315+nyan-tech-learn-more@users.noreply.github.com>
2024-03-25 14:58:05 +0800

    Create 2024Task#09_HW_02.py

---
+++ 2024Task#09_HW_02.py
@@ -0,0 +1,23 @@
+繳交作業直接貼上完整的 Python 程式碼即可（不用上傳 .py 檔案），不用附上題目、標題設定成「2024 Task#09 作業 02」：
+
+#練習：定義一個計算質數的函數。
+
+❏ Requirements：
+
+1. 讓使用者輸入一個整數 n
+
+2. 請分別定義兩個 Function: isPrime(n) 回傳 n 是否為質數、primes(n) 回傳小於 n 質數
+
+❏ Sample Input： 20 ← 使用者輸入
+
+❏ Sample Output： 2, 3, 5, 7, 11, 13, 17, 19 ← 畫面輸出
+
+❏ Sample Code：
+
+x = input() # 20
+
+'''
+Your Code
+'''
+
+print(f) # 2, 3, 5, 7, 11, 13, 17, 19
